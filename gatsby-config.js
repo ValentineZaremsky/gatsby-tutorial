ca196eb0b4b293fba5d40b9d418ab4f5670bbb38
Valentine Zaremsky <designer@equatorcasino.com>
2020-12-29 21:18:38 +0200

    fill pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,13 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -19,6 +23,13 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`
       }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`
