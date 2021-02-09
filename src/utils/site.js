c0ad1a884da23eb49a7d93f9db9964488f325263
Valentine Zaremsky <designer@equatorcasino.com>
2021-02-09 21:40:53 +0200

    add footer icons

--- src/utils/site.js
+++ src/utils/site.js
@@ -1,10 +1,14 @@
 export const site = {
-  siteName:  "Andromeda",
-  siteURL:   "andromeda-blog.netlify.app",
-  siteEmail: "info@andromeda.com",
-  siteGeo:   "https://goo.gl/maps/fgtLEQoDsQjTDTfb7",
-  siteAddr1: "15 Vasilieva,",
-  siteAddr2: "Kamyanets-Podilsky, 32300,",
-  siteAddr3: "Ukraine",
-  siteTel:   "+380 66 777 88 99"
+  siteName:   "Andromeda",
+  siteURL:    "andromeda-blog.netlify.app",
+  siteEmail1: "info@andromeda.com",
+  siteEmail2: "",
+  siteEmail3: "",
+  siteTel1:   "+380 66 777 88 99",
+  siteTel2:   "",
+  siteTel3:   "",
+  siteGeo:    "https://goo.gl/maps/fgtLEQoDsQjTDTfb7",
+  siteAddr1:  "15 Vasilieva,",
+  siteAddr2:  "Kamyanets-Podilsky,",
+  siteAddr3:  "32300, Ukraine"
 };
